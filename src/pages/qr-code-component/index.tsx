46f2b4343f54b02b4ffa7f57ec1002e267c0a0da
sashauly <sashauly.code@gmail.com>
2024-10-16 17:57:55 +0300

    add merkdown render for all existing projects

--- src/pages/qr-code-component/index.tsx
+++ src/pages/qr-code-component/index.tsx
@@ -1,15 +1,28 @@
 import IFrameComponent from "@/components/IFrameComponent";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import fs from "fs";
+import path from "path";
 
 export const qrCodeComponentUrl = "qr-code-component";
+const qrCodeComponentMarkup = `/${qrCodeComponentUrl}/index.html`;
+const qrCodeComponentMarkdown = `/${qrCodeComponentUrl}/README.md`;
 
-export default function QrCodeComponentPage() {
+const filePath = path.join(process.cwd(), "/public", qrCodeComponentMarkdown);
+
+export const getStaticProps: GetStaticProps = (async () => {
+  const fileContent = await fs.readFileSync(filePath, "utf8");
+  return { props: { fileContent } };
+}) satisfies GetStaticProps<{ fileContent: string }>;
+
+export default function QrCodeComponentPage({
+  fileContent,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div id={qrCodeComponentUrl}>
       <h3>{qrCodeComponentUrl}</h3>
-      <IFrameComponent
-        src={`/${qrCodeComponentUrl}/index.html`}
-        title={qrCodeComponentUrl}
-      />
+      <IFrameComponent src={qrCodeComponentMarkup} title={qrCodeComponentUrl} />
+      <MarkdownRenderer fileContent={fileContent} />
     </div>
   );
 }
--- src/pages/social-links-profile/index.tsx
+++ src/pages/social-links-profile/index.tsx
@@ -1,15 +1,35 @@
 import IFrameComponent from "@/components/IFrameComponent";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+import fs from "fs";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import path from "path";
 
 export const socialLinksProfileUrl = "social-links-profile";
+const socialLinksProfileMarkup = `/${socialLinksProfileUrl}/index.html`;
+const socialLinksProfileMarkdown: string = `/${socialLinksProfileUrl}/README.md`;
 
-export default function SocialLinksProfilePage() {
+const filePath = path.join(
+  process.cwd(),
+  "/public",
+  socialLinksProfileMarkdown
+);
+
+export const getStaticProps: GetStaticProps = (async () => {
+  const fileContent = await fs.readFileSync(filePath, "utf8");
+  return { props: { fileContent } };
+}) satisfies GetStaticProps<{ fileContent: string }>;
+
+export default function SocialLinksProfilePage({
+  fileContent,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div id={socialLinksProfileUrl}>
       <h3>{socialLinksProfileUrl}</h3>
       <IFrameComponent
-        src={`/${socialLinksProfileUrl}/index.html`}
+        src={socialLinksProfileMarkup}
         title={socialLinksProfileUrl}
       />
+      <MarkdownRenderer fileContent={fileContent} />
     </div>
   );
 }
